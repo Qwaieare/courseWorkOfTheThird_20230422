7619e2ca75c8f062d637421a4cc71b4f31ee7eee
Qwaieare <gb-grand@mail.ru>
2023-04-22 13:15:56 +0300

    initial commit for courseWorkOfTheThird_20230422

---
+++ src/main/java/skypro/internetshopsocks/CourseWorkOfTheThird20230422Application.java
@@ -0,0 +1,13 @@
+package skypro.internetshopsocks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseWorkOfTheThird20230422Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseWorkOfTheThird20230422Application.class, args);
+    }
+
+}
---
+++ src/test/java/skypro/internetshopsocks/CourseWorkOfTheThird20230422ApplicationTests.java
@@ -0,0 +1,13 @@
+package skypro.internetshopsocks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseWorkOfTheThird20230422ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
